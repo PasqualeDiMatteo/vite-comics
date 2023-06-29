24c72281e86184eb088cb8916fd9569ef75c562f
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-29 18:03:28 +0200

    Added AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+    <div class="background">
+        <div class="container">
+            <div class="content">
+                <h1>Content goes here</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: white;
+}
+
+.background {
+    background-color: #1c1c1c;
+}
+
+.content {
+    display: flex;
+    align-items: center;
+    height: 150px;
+}
+</style>
