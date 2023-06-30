3689b97d35fd1a324052fb532722699412270371
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-30 16:29:26 +0200

    Added ComicCard

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    props: {
+        image: String,
+        title: String
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src=this.image :alt=this.title>
+        <h3>{{ this.title }}</h3>
+    </div>
+</template>
+<style scoped lang="scss">
+.card {
+    flex-basis: calc(100%/6);
+    margin: 30px 0;
+
+
+    img {
+        display: block;
+        object-fit: scale-down;
+        object-position: top;
+        width: 100%;
+        height: 150px;
+        clip-path: inset(0 26% 0 26%);
+        cursor: pointer;
+    }
+
+    h3 {
+        margin-top: 10px;
+        margin-left: 26%;
+        color: white;
+        font-size: 12px;
+    }
+}
+</style>
